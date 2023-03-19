14dee4917b309f8b01cef451fc5454401de11002
Daniel Hampikian <danielhampikian@gmail.com>
2023-03-19 15:27:02 -0700

    pruned games

--- games/php_card_game/password.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("user_info").innerHTML = "username taken or password incorrect";
-document.getElementById("password_info").innerHTML = "username taken or password incorrect";
-
-
